f3ff5a64e715d12e7cf2641f9113a4fad43afae1
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2024-10-18 01:31:38 -0700

    new event button

--- app/(private)/events/page.tsx
+++ app/(private)/events/page.tsx
@@ -1,11 +1,20 @@
-import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { CalendarPlus } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 export default function EventsPage() {
   return (
     <>
-      <h1>Events</h1>;
-      <UserButton />
+      <div>
+        <h1>Events</h1>
+        <Button asChild>
+          <Link href="/events/new">
+            <CalendarPlus className="mr-4 size-6 " />
+            New event
+          </Link>
+        </Button>
+      </div>
     </>
   );
 }
