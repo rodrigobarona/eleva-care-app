a2d2b0c3c516e4d4d4932419d64b6f33657fe28c
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2024-10-18 12:17:14 -0700

    add event new page

--- app/(private)/events/page.tsx
+++ app/(private)/events/page.tsx
@@ -1,13 +1,24 @@
 import { Button } from "@/components/ui/button";
-import { CalendarPlus } from "lucide-react";
+import { db } from "@/drizzle/db";
+import { auth } from "@clerk/nextjs/server";
+import { CalendarPlus, CalendarRange } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
-export default function EventsPage() {
+export default async function EventsPage() {
+  const { userId, redirectToSignIn } = auth();
+
+  if (userId == null) return redirectToSignIn;
+  const events = await db.query.EventTable.findMany({
+    where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
+    orderBy: ({ createdAt }, { desc }) => desc(createdAt),
+  });
   return (
     <>
-      <div>
-        <h1>Events</h1>
+      <div className="flex gap-4 items-baseline">
+        <h1 className="text-3xl lg:text-4xl xl:text-5xl font-semibold mb-6">
+          Events
+        </h1>
         <Button asChild>
           <Link href="/events/new">
             <CalendarPlus className="mr-4 size-6 " />
@@ -15,6 +26,21 @@ export default function EventsPage() {
           </Link>
         </Button>
       </div>
+      {events.length > 0 ? (
+        <h1>Events</h1>
+      ) : (
+        <div className="flex flex-col items-center gap-6 ">
+          <CalendarRange className="size-16 mx-auto" />
+          You you do not have an event yet. Create your first evebnt to get
+          started!
+          <Button size="lg" className="text-lg" asChild>
+            <Link href="/events/new">
+              <CalendarPlus className="mr-4 size-6 " />
+              New event
+            </Link>
+          </Button>
+        </div>
+      )}
     </>
   );
 }
