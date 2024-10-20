632dc9722b23e1e22016748171ee8d35ecfb351d
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2024-10-19 23:04:04 -0700

    create the form event

--- app/(private)/events/new/page.tsx
+++ app/(private)/events/new/page.tsx
@@ -1,8 +1,19 @@
+import { EventForm } from "@/components/molecules/EventForm";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import React from "react";
 
 const NewEventPage = () => {
   // Your component logic here
-  return <div>{/* Your JSX here */}</div>;
+  return (
+    <Card className="max-w-md max-auto">
+      <CardHeader>
+        <CardTitle>New Event</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <EventForm />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default NewEventPage;
---
+++ components/molecules/EventForm.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { eventFormSchema } from "@/schema/events";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+
+export function EventForm() {
+  const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: {
+      isActive: true,
+      durationInMinutes: 30,
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Event Name</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormDescription>
+                The name user will see when booking
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+}
