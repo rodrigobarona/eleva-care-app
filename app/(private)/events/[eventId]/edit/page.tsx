d0316f795b16c70c1193698a680187bb8d845e30
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2024-10-20 03:53:20 -0700

    fix error event description

--- app/(private)/events/[eventId]/edit/page.tsx
+++ app/(private)/events/[eventId]/edit/page.tsx
@@ -2,6 +2,7 @@ import { EventForm } from "@/components/molecules/EventForm";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { db } from "@/drizzle/db";
 import { auth } from "@clerk/nextjs/server";
+import { notFound } from "next/navigation";
 import React from "react";
 
 export default async function EditEventPage({
@@ -18,13 +19,17 @@ export default async function EditEventPage({
       and(eq(clerkUserId, userId), eq(id, eventId)),
   });
 
+  if (event == null) return notFound();
+
   return (
     <Card className="max-w-md max-auto">
       <CardHeader>
         <CardTitle>Edit Event</CardTitle>
       </CardHeader>
       <CardContent>
-        <EventForm event={event} />
+        <EventForm
+          event={{ ...event, description: event.description || undefined }}
+        />
       </CardContent>
     </Card>
   );
