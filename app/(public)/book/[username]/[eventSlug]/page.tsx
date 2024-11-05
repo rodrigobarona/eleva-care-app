d931bd5491b52fadd7be12de2a9164da7115589d
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2024-11-05 03:15:21 -0800

    fix missing variable

--- app/(public)/book/[username]/[eventSlug]/page.tsx
+++ app/(public)/book/[username]/[eventSlug]/page.tsx
@@ -86,6 +86,7 @@ export default async function BookEventPage({
           validTimes={validTimes}
           eventId={event.id}
           clerkUserId={user.id}
+          username={username}
         />
       </CardContent>
       <CardFooter></CardFooter>
