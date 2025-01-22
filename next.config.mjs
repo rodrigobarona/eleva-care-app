5f63bd8f37a2f845e8106f7c62c5d36967135df8
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2025-01-21 21:04:02 -0800

    review updates to start

--- next.config.mjs
+++ next.config.mjs
@@ -22,15 +22,6 @@ const nextConfig = {
       dynamic: 0,
     },
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.ignoreWarnings = [
-        { module: /node_modules\/node-fetch\/lib\/index\.js/ },
-        { module: /node_modules\/punycode\// },
-      ];
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
