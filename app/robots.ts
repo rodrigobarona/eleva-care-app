3121c699748e3225b340e81e5e4cdeabe24a8924
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2025-03-03 22:31:15 -0800

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        '/home/',
+        '/account/',
+        '/admin/',
+        '/api/',
+        '/onboarding/',
+        '/unauthorized/',
+        '/appointments/',
+        '/customers/',
+        '/events/',
+        '/expert/',
+        '/schedule/',
+        '/data/',
+      ],
+    },
+    // sitemap: 'https://eleva.care/sitemap.xml',
+  };
+}
