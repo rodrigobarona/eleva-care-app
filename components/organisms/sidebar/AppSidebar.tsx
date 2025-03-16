8c8b0571f32b28c39980d7e5ed7e3df455c8c954
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2025-03-15 17:33:40 -0700

    remove icon unused

--- components/organisms/sidebar/AppSidebar.tsx
+++ components/organisms/sidebar/AppSidebar.tsx
@@ -14,7 +14,7 @@ import {
   SidebarMenuItem,
 } from '@/components/organisms/sidebar/sidebar';
 import { useUser } from '@clerk/nextjs';
-import { Clock, ExternalLink, Leaf, LifeBuoy, type LucideIcon, User, Users } from 'lucide-react';
+import { Clock, ExternalLink, Leaf, LifeBuoy, type LucideIcon, User } from 'lucide-react';
 import { Link as LinkIcon } from 'lucide-react';
 import Link from 'next/link';
 import { Suspense } from 'react';
