d5203c331d70300d600cc08c612a067f85456391
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2025-05-03 23:02:42 -0700

    remove contente collections

--- .content-collections/generated/index.d.ts
+++
@@ -1,7 +0,0 @@
-import configuration from "../../content-collections.ts";
-import { GetTypeByName } from "@content-collections/core";
-
-export type About = GetTypeByName<typeof configuration, "about">;
-export declare const allAbouts: Array<About>;
-
-export {};
