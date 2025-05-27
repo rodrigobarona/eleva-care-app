b3c284d3fd0b5d884439b773177cb9b37fdd132b
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2025-05-26 19:08:55 -0700

    remove tremor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,6 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     container: {
