75c541a1f28cbabdbafdafc53d4d0091b343050f
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2025-09-29 15:18:02 +0100

    Revert "Update profile picture handling in ProfileInfo component"
    This reverts commit 9a6976abfc5bb807713ef368e0b51206981fd79c.

--- app/[locale]/(public)/[username]/page.tsx
+++ app/[locale]/(public)/[username]/page.tsx
@@ -234,7 +234,7 @@ async function ProfileInfo({
       <div className="space-y-6">
         <div className="relative aspect-[18/21] w-full overflow-hidden rounded-lg">
           <Image
-            src={clerkUserImageUrl}
+            src={profile?.profilePicture || clerkUserImageUrl}
             alt={clerkUserFullName || 'Profile picture'}
             fill
             className="object-cover"
